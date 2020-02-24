05e8e01bc02700ee7fd4111990f138c85356a0bb
Ataor <slyguggi@gmail.com>
2020-02-24 15:51:02 +0100

    connection test

---
+++ Assets/Scripts/SimpleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SimpleController : NetworkBehaviour
+{
+    [SerializeField] private float speed = 10.0f;
+
+    private void Update()
+    {
+        if (isLocalPlayer && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+        {
+            transform.position += Time.deltaTime * speed *
+                                  (Vector3.right * Input.GetAxis("Horizontal") +
+                                   Vector3.up * Input.GetAxis("Vertical"));
+        }
+    }
+}
